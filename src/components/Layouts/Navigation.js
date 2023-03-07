e59f0c897e5ce130f56d9dbf74351ab66d152d99
funayamateppei <t.funayama0217@gmail.com>
2023-03-07 11:50:08 +0900

    ログイン後、新規登録後にとぶページをdashboardからhomeに変更 Navigationも変更

--- src/components/Layouts/Navigation.js
+++ src/components/Layouts/Navigation.js
@@ -23,19 +23,13 @@ const Navigation = ({ user }) => {
             <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex justify-between h-16">
                     <div className="flex">
-                        {/* Logo */}
-                        <div className="flex-shrink-0 flex items-center">
-                            <Link href="/dashboard">
-                                <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
-                            </Link>
-                        </div>
 
                         {/* Navigation Links */}
                         <div className="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                             <NavLink
-                                href="/dashboard"
-                                active={router.pathname === '/dashboard'}>
-                                Dashboard
+                                href="/home"
+                                active={router.pathname === '/home'}>
+                                Home
                             </NavLink>
                         </div>
                     </div>
@@ -108,9 +102,9 @@ const Navigation = ({ user }) => {
                 <div className="block sm:hidden">
                     <div className="pt-2 pb-3 space-y-1">
                         <ResponsiveNavLink
-                            href="/dashboard"
-                            active={router.pathname === '/dashboard'}>
-                            Dashboard
+                            href="/home"
+                            active={router.pathname === '/home'}>
+                            Home
                         </ResponsiveNavLink>
                     </div>
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -17,9 +17,9 @@ export default function Home() {
                 <div className={styles.buttonContainer}>
                     {user ? (
                         <Link
-                            href="/dashboard"
+                            href="/home"
                             className={styles.button}>
-                            Dashboard
+                            Home
                         </Link>
                     ) : (
                         <>
