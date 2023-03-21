a70e67dfa3ce81d8434d682071af511306a35268
funayamateppei <t.funayama0217@gmail.com>
2023-03-22 04:30:45 +0900

    thread詳細ページエンドポイントからデータ取得実装

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -2,7 +2,6 @@ import React from 'react'
 import styles from '@/styles/follows.module.css'
 
 const FollowButton = ({ data }) => {
-    console.log(data)
     return (
         <>
             {data.is_following ? (
--- src/components/Image.js
+++ src/components/Image.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Image = ({ src, alt, style }) => {
     return (
-        <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${src}`} alt={alt} className={style} />
+        <img
+            src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${src}`}
+            alt={alt}
+            className={style}
+        />
     )
 }
 
