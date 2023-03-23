30c36fc2e7eee3334aed6530a46d4e68a61ddfbc
funayamateppei <t.funayama0217@gmail.com>
2023-03-23 20:20:58 +0900

    コメントページ作成中

--- src/pages/thread/comment/[id].js
+++ src/pages/thread/comment/[id].js
@@ -1,56 +1,70 @@
 import React from 'react'
 
 import { useAuth } from '@/hooks/auth'
+import useSWR from 'swr'
+import { useEffect } from 'react'
 import axios from '@/lib/axios'
 
-// const Comment = ({ id, commentData }) => {
-const Comment = ({ id, commentData }) => {
-  const { user } = useAuth({ middleware: 'auth' })
-  
-  const hoge = axios.get('/api/threads/comments/1')
-  console.log(hoge)
+const Comment = ({ id, datalist }) => {
+    const { user } = useAuth({ middleware: 'auth' })
 
-    console.log(commentData)
-    console.log(id)
-    return <div>Comment</div>
+    // CSRで最新の情報を取得
+    const fetcher = url => {
+        return axios(url).then(response => response.data)
+    }
+    const apiUrl = `/api/threads/comments/${id}`
+    const { data: data, mutate } = useSWR(apiUrl, fetcher, {
+        fallbackData: datalist,
+    })
+    useEffect(() => {
+        mutate()
+    }, [])
+
+    console.log(data)
+
+    return (
+        <div>
+            <button>thread comment</button>
+        </div>
+    )
 }
 
-// export async function getAllThreadIds() {
-//     const response = await axios.get('/api/threads/ids')
-//     const threads = await response.data
-//     return await threads.map(thread => {
-//         return {
-//             params: {
-//                 id: String(thread.id),
-//             },
-//         }
-//     })
-// }
+export async function getAllThreadIds() {
+    const response = await axios.get('/api/threads/ids')
+    const threads = await response.data
+    return await threads.map(thread => {
+        return {
+            params: {
+                id: String(thread.id),
+            },
+        }
+    })
+}
 
-// export async function getStaticPaths() {
-//     const paths = await getAllThreadIds()
-//     return {
-//         paths,
-//         fallback: true,
-//     }
-// }
+export async function getStaticPaths() {
+    const paths = await getAllThreadIds()
+    return {
+        paths,
+        fallback: true,
+    }
+}
 
-// export async function getThreadCommentData(id) {
-//     const response = await axios.get(`/api/threads/comments/${id}`)
-//     const data = await response.data
-//     return data
-// }
+export async function getThreadCommentData(id) {
+    const response = await axios.get(`/api/threads/comments/${id}`)
+    const data = await response.data
+    return data
+}
 
-// export async function getStaticProps({ params }) {
-//     const data = await getThreadCommentData(params.id)
-//     const id = data.id
-//     return {
-//         props: {
-//             id: id,
-//             commentData: data,
-//         },
-//         revalidate: 3,
-//     }
-// }
+export async function getStaticProps({ params }) {
+    const data = await getThreadCommentData(params.id)
+    const id = data.threadData.id
+    return {
+        props: {
+            id: id,
+            datalist: data,
+        },
+        revalidate: 3,
+    }
+}
 
 export default Comment
