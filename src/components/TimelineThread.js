4eef3b97f057cc75590ab4af9daaf2f7f090bb48
funayamateppei <t.funayama0217@gmail.com>
2023-03-27 17:06:02 +0900

    timeline作成 無限スクロール実装

---
+++ src/components/TimelineThread.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import styles from '@/styles/components/TimelineThread.module.css'
+
+import Link from 'next/link'
+import Image from './Image'
+
+const TimelineThread = ({ thread }) => {
+    // 日付のフォーマットを変更
+    const timestamp = thread.updated_at
+    const date = new Date(timestamp)
+    const formatted_date = `${(date.getMonth() + 1)
+        .toString()
+        .padStart(2, '0')}/${date
+        .getDate()
+        .toString()
+        .padStart(2, '0')} ${date
+        .getHours()
+        .toString()
+        .padStart(2, '0')}:${date.getMinutes().toString().padStart(2, '0')}`
+    console.log(formatted_date)
+
+    return (
+        <div className={styles.threadBox}>
+            <Link href={`/thread/${thread.id}`}>
+                <div className={styles.threadImageBox}>
+                    <Image
+                        src={thread.thread_images[0].path}
+                        alt="thread_image"
+                        style="w-full h-full object-contain"
+                    />
+                </div>
+
+                <div className={styles.userInfo}>
+                    <Image
+                        src={
+                            thread?.user?.icon_path
+                                ? thread.user.icon_path
+                                : 'icon.png'
+                        }
+                        alt="icon"
+                        style="h-8 w-8 rounded-full border border-gray-400 ml-2"
+                    />
+                    <p className={styles.username}>{thread?.user?.name}</p>
+                </div>
+                <div className={styles.statusBox}>
+                    <div className={styles.statusCount}>
+                        <p className={styles.date}>{formatted_date}</p>
+                        <div className={styles.flex}>
+                            <div className={styles.status}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-4 h-4">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0111.186 0z"
+                                    />
+                                </svg>
+                                <p>{thread.bookmarked_threads_count}</p>
+                            </div>
+                            <div className={styles.status}>
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-4 h-4">
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"
+                                    />
+                                </svg>
+                                <p>{thread.liked_threads_count}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default TimelineThread
--- src/pages/timeline/index.js
+++ src/pages/timeline/index.js
@@ -1,22 +1,108 @@
 import React from 'react'
 import styles from '@/styles/timeline.module.css'
+
 import Layout from '@/components/Layouts/Layout'
 import Head from 'next/head'
+import TimelineThread from '@/components/TimelineThread'
 import FooterTabBar from '@/components/FooterTabBar'
+
 import { useAuth } from '@/hooks/auth'
+import useSWR from 'swr'
+import axios from '@/lib/axios'
+import { useEffect, useState } from 'react'
 
 const Timeline = () => {
-    const { user } = useAuth({ middleware: 'guest' })
+    const { user } = useAuth({ middleware: 'auth' })
+
+    const PAGE_SIZE = 8
+
+    const [currentPage, setCurrentPage] = useState(1)
+    const [isLoading, setIsLoading] = useState(true)
+    const [threads, setThreads] = useState([])
+
+    // CSRで最新の情報を取得
+    const fetcher = url => {
+        return axios(url).then(response => response.data)
+    }
+
+    const { data, error } = useSWR(
+        `/api/timeline?page=${currentPage}&size=${PAGE_SIZE}`,
+        fetcher,
+    )
+
+    useEffect(() => {
+        if (data) {
+            setThreads(prevThreads => [...prevThreads, ...data.data])
+            setIsLoading(false)
+        }
+    }, [data])
+
+    const handleScroll = () => {
+        const scrollTop =
+            (document.documentElement && document.documentElement.scrollTop) ||
+            document.body.scrollTop
+        const scrollHeight =
+            (document.documentElement &&
+                document.documentElement.scrollHeight) ||
+            document.body.scrollHeight
+        const clientHeight =
+            document.documentElement.clientHeight || window.innerHeight
+        const scrolledToBottom =
+            Math.ceil(scrollTop + clientHeight) >= scrollHeight
+
+        if (scrolledToBottom && data && data.next_page_url) {
+            setCurrentPage(prevPage => prevPage + 1)
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll)
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [data])
+
+    console.log(data)
+
+    if (isLoading) {
+        return (
+            <div className={styles.flexContainer}>
+                <img src="loading.gif" alt="loading" />
+            </div>
+        )
+    }
+
+    if (error) {
+        return (
+            <div className={styles.flexContainer}>
+                <p>エラーが発生しました</p>
+            </div>
+        )
+    }
 
     return (
         <Layout>
             <Head>
                 <title>Exhibit</title>
             </Head>
-            <div className={styles.container}>
-                <div className={styles.content}>{/* ページコンテンツ */}</div>
-                <FooterTabBar user={user} />
+            <div className={styles.header}>
+                <p>フォロー中</p>
             </div>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.itemBox}>
+                        {threads
+                            ? threads.map((thread, index) => (
+                                  <TimelineThread key={index} thread={thread} />
+                              ))
+                            : null}
+                    </div>
+                    {!data.next_page_url && (
+                        <div className={styles.noMoreData}>
+                            これ以上ありません
+                        </div>
+                    )}
+                </div>
+            </div>
+            <FooterTabBar user={user} />
         </Layout>
     )
 }
