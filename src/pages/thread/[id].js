086b6d506b4cad5114d0c8392f89a13afb9e761e
funayamateppei <t.funayama0217@gmail.com>
2023-04-02 12:43:40 +0900

    ログインユーザーの投稿の場合は編集ボタンを表示するように変更

--- src/pages/thread/[id].js
+++ src/pages/thread/[id].js
@@ -121,12 +121,6 @@ const Thread = ({ id, threadData }) => {
         }
     }
 
-    if (user && data) {
-        if (user.id === data.user.id) {
-            router.push(`/profile/thread/${data.id}`)
-        }
-    }
-
     if (threadData === null) {
         return (
             <div className={styles.flexContainer}>
@@ -183,21 +177,30 @@ const Thread = ({ id, threadData }) => {
                                         </div>
                                     </Link>
                                 ) : null}
-                                <div>
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                        strokeWidth={1.5}
-                                        stroke="currentColor"
-                                        className="w-6 h-6">
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            d="M8.625 12a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                                        />
-                                    </svg>
-                                </div>
+                                {user && data ? (
+                                    user.id === data.user.id ? (
+                                        <div>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth={1.5}
+                                                stroke="currentColor"
+                                                className="w-6 h-6"
+                                                onClick={() =>
+                                                    router.push(
+                                                        `/profile/thread/${data.id}`,
+                                                    )
+                                                }>
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+                                                />
+                                            </svg>
+                                        </div>
+                                    ) : null
+                                ) : null}
                             </div>
 
                             <div className={styles.sliderBox}>
